0ee6a9786a72e1d391426d3500c8382113b72701
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 21:22:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{|item,index|
+  puts "#{index+1} #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+  def summon_captain_planet(array)
+    array = array.map{|i| i.capitalize + "!"}
+    return array 
+  end
+
+def long_planeteer_calls(array)
+  array.any?{|n| n.length >4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |n|
+    if cheese_types.include?(n)
+      return n
+    end 
+  end 
+  return nil
+  
 end
